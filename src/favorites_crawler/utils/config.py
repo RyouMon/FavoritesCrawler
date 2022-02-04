d213d6a1d78857dd566741111a8600cbb56dad06
Wen <wenslife@outlook.com>
2022-02-04 17:14:42 +0800

    refactor: change structure of config file

--- src/favorites_crawler/utils/config.py
+++ src/favorites_crawler/utils/config.py
@@ -3,21 +3,20 @@ import os
 import yaml
 
 config_path = os.path.expanduser('~/.favorites_crawler')
+config_file = os.path.join(config_path, 'config.yml')
 if not os.path.exists(config_path):
     os.mkdir(config_path)
 
 
-def load_config(config_name):
+def load_config():
     """Load config from user home"""
-    config_file = os.path.join(config_path, config_name)
     if not os.path.exists(config_file):
         return {}
     with open(config_file, encoding='utf8') as f:
         return yaml.safe_load(f)
 
 
-def dump_config(config_name, data):
+def dump_config(data):
     """Dump config data to user home"""
-    config_file = os.path.join(config_path, config_name)
     with open(config_file, 'w', encoding='utf8') as f:
         yaml.safe_dump(data, f)

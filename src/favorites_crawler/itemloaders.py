ee3faca2cf807885e0b54a2db860f7ddd41ab8ff
Wen <wenslife@outlook.com>
2022-02-11 20:26:45 +0800

    feat: add NHentaiGalleryItemLoader

--- src/favorites_crawler/itemloaders.py
+++ src/favorites_crawler/itemloaders.py
@@ -1,27 +1,35 @@
 from itemloaders import ItemLoader
+from itemloaders.processors import Join
 
 from favorites_crawler import items
-from favorites_crawler import processors as pc
+from favorites_crawler.processors import take_first, identity, filter_pixiv_tags
 
 
 class PixivIllustItemLoader(ItemLoader):
     """Pixiv Illust Loader"""
     default_item_class = items.PixivIllustItem
-    default_output_processor = pc.take_first
+    default_output_processor = take_first
 
-    original_image_urls_out = pc.identity
-    tags_out = pc.filter_pixiv_tags
+    original_image_urls_out = identity
+    tags_out = filter_pixiv_tags
 
 
 class YanderePostItemLoader(ItemLoader):
     """Yandere Post Loader"""
     default_item_class = items.YanderePostItem
-    default_output_processor = pc.take_first
+    default_output_processor = take_first
+
+
+class NHentaiGalleryItemLoader(ItemLoader):
+    default_item_class = items.NHentaiGalleryItem
+    default_output_processor = take_first
+
+    title_out = Join('')
 
 
 class LemonPicPostItemLoader(ItemLoader):
     default_item_class = items.LemonPicPostItem
-    default_output_processor = pc.take_first
+    default_output_processor = take_first
 
-    image_urls_out = pc.identity
-    tags_out = pc.identity
+    image_urls_out = identity
+    tags_out = identity

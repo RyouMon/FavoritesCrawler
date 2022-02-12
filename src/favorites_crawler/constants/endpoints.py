403be35205bea7f72042e23211f4789a3a056e8e
Wen <wenslife@outlook.com>
2022-02-12 12:21:15 +0800

    feat: add constant LEMON_PIC_USER_FAVORITES_URL

--- src/favorites_crawler/constants/endpoints.py
+++ src/favorites_crawler/constants/endpoints.py
@@ -6,3 +6,4 @@ PIXIV_AUTH_TOKEN_URL = "https://oauth.secure.pixiv.net/auth/token"
 YANDERE_POST_URL = 'https://yande.re/post.json'
 
 LEMON_PIC_LOGIN_URL = 'https://www.lmmpic.com/wp-login.php'
+LEMON_PIC_USER_FAVORITES_URL = 'https://www.lmmpic.com/user-center#usertab4'

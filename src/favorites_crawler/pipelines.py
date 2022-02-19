332478ce866527015919256f0ea6f83d2f43acdc
Wen <wenslife@outlook.com>
2022-02-19 18:34:38 +0800

    feat: add CollectionFilePipeline

--- src/favorites_crawler/pipelines.py
+++ src/favorites_crawler/pipelines.py
@@ -45,3 +45,13 @@ class YandreFilesPipeline(FilesPipeline):
 
     def file_path(self, request, response=None, info=None, *, item=None):
         return os.path.join('Yandere', item.get_filename())
+
+
+class CollectionFilePipeline(FilesPipeline):
+
+    def get_media_requests(self, item, info):
+        item_dict = ItemAdapter(item).asdict()
+        return (Request(url) for url in item_dict.get('image_urls', ()))
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        return item.get_filename(request.url)

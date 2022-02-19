3850e8776639d25c8a2af2541b9235a2f4233337
Wen <wenslife@outlook.com>
2022-02-19 22:11:27 +0800

    feat(processors): add get_nhentai_id

--- src/favorites_crawler/processors.py
+++ src/favorites_crawler/processors.py
@@ -1,3 +1,5 @@
+import re
+
 from itemloaders.processors import TakeFirst, Identity
 
 from favorites_crawler.constants.blacklists import PIXIV_TAG_KEYWORD_BLACKLIST
@@ -15,3 +17,11 @@ def filter_pixiv_tags(tags):
                     return False
         return True
     return [tag for tag in filter(filter_tag, tags)]
+
+
+def get_nhentai_id(url):
+    """Get comic ID from comic url"""
+    match = re.match(r'https.+g/(\d+)/', url)
+    if not match:
+        return ''
+    return match.group(1)

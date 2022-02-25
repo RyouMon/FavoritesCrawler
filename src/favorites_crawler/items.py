e6dd8fdc4401a6bf64dfdc3baff6da758e296fc3
Wen <wenslife@outlook.com>
2022-02-25 21:32:51 +0800

    refactor(items): PixivIllustItem extends BaseItem BREAKING CHANGE: Affects PixivSpider, PixivIllustItemLoader

--- src/favorites_crawler/items.py
+++ src/favorites_crawler/items.py
@@ -40,22 +40,17 @@ class BaseItem:
 
 
 @dataclass
-class PixivIllustItem:
-    """Pixiv Illust"""
-    id: int = field(default=None)
-    title: str = field(default=None)
-    tags: list = field(default_factory=list)
-    referer: str = field(default=None)
-    original_image_urls: list = field(default=None)
+class PixivIllustItem(BaseItem):
 
-    def get_filename(self, pk, ext):
-        """pk is file_id, ext is file extension"""
-        tags = ' '.join(map(
-            lambda s: drop_illegal_characters(s).replace(' ', '_'),
-            (tag.get('translated_name') or tag.get('name', '') for tag in self.tags)
-        ))
-        title = drop_illegal_characters(self.title)
-        return f'{pk} {title} [{tags}].{ext}'
+    def get_folder_prefix(self):
+        return ''
+
+    def get_folder_subfix(self):
+        return ''
+
+    def get_folder_name(self):
+        return 'Pixiv'
+
 
 
 @dataclass

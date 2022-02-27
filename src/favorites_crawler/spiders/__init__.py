23382fa49543b927cb7993aa4582df7ab11ab68e
Wen <wenslife@outlook.com>
2022-02-27 17:43:45 +0800

    refactor(spiders): Clean BaseSpider

--- src/favorites_crawler/spiders/__init__.py
+++ src/favorites_crawler/spiders/__init__.py
@@ -7,16 +7,7 @@ from abc import ABCMeta
 
 from scrapy.spiders import CrawlSpider
 
-from favorites_crawler.utils.config import load_config
-
 
 class BaseSpider(CrawlSpider, metaclass=ABCMeta):
 
     custom_settings = {}
-
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        spider = super().from_crawler(crawler, *args, **kwargs)
-        user_config = load_config().get(cls.name, {})
-        spider.custom_settings.update(user_config)
-        return spider

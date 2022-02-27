1cdeef44df268c98fb42dc7b5e3d59c0b51ebcaa
Wen <wenslife@outlook.com>
2022-02-27 16:01:57 +0800

    feat(spiders): Add base class for all spiders, base spider using user config update custom settings.

--- src/favorites_crawler/spiders/__init__.py
+++ src/favorites_crawler/spiders/__init__.py
@@ -2,3 +2,20 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+from abc import ABCMeta
+
+from scrapy.spiders import CrawlSpider
+
+from favorites_crawler.utils.config import load_config
+
+
+class BaseSpider(CrawlSpider, metaclass=ABCMeta):
+
+    custom_settings = {}
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super().from_crawler(cls, crawler, *args, **kwargs)
+        user_config = load_config().get(cls.name, {})
+        spider.custom_settings.update(user_config)

17bcb39284f6fc30e623ee6f4f87eb7f4e184b9e
Wen <wenslife@outlook.com>
2022-02-27 10:46:03 +0800

    refactor(pipelines): Remove YandreFilesPipeline BREAKING CHANGE: Affects YandereSpider

--- src/favorites_crawler/pipelines.py
+++ src/favorites_crawler/pipelines.py
@@ -3,21 +3,9 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-import os
-
 from scrapy import Request
 from scrapy.pipelines.files import FilesPipeline
 from itemadapter import ItemAdapter
-
-
-class YandreFilesPipeline(FilesPipeline):
-
-    def get_media_requests(self, item, info):
-        item_dict = ItemAdapter(item).asdict()
-        return [Request(item_dict['file_url'])]
-
-    def file_path(self, request, response=None, info=None, *, item=None):
-        return os.path.join('Yandere', item.get_filename())
 
 
 class CollectionFilePipeline(FilesPipeline):

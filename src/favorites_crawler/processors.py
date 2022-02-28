5a0c3cad7888f9ece2fb389a694f7e55db1eb081
Wen <wenslife@outlook.com>
2022-02-28 21:03:10 +0800

    feat(processors): Add original_url_from_nhentai_thumb_url

--- src/favorites_crawler/processors.py
+++ src/favorites_crawler/processors.py
@@ -17,3 +17,12 @@ def get_nhentai_id(url):
 
 def replace_space_with_under_scope(text):
     return '_'.join(text.split())
+
+
+def original_url_from_nhentai_thumb_url(url):
+    """replace 't' with 'i' in domain, remove 't' from filename.
+    'https://t2.nhentai.net/galleries/154829234522/4t.png'
+             |                                      |
+    'https://i2.nhentai.net/galleries/154829234522/4.png'
+    """
+    return re.sub(r'https://t(.+/\d+)t(\..+)', r'https://i\g<1>\g<2>', url)

f25933d1ff30d22d5ad79664235122f60fef8c6a
Wen <wenslife@outlook.com>
2022-04-23 17:32:26 +0800

    pref(utils): handle exception raised by load_cookie

--- src/favorites_crawler/utils/cookies.py
+++ src/favorites_crawler/utils/cookies.py
@@ -7,7 +7,11 @@ cookie_home = os.path.expanduser('~/.favorites_crawler')
 
 def load_cookie(domain):
     """Load 'Netscape HTTP Cookie File' as dict"""
-    filename = os.path.join(cookie_home, f'{domain}_cookies.txt')
-    cookiejar = MozillaCookieJar()
-    cookiejar.load(filename)
-    return {getattr(c, 'name'): getattr(c, 'value') for c in cookiejar}
+    filename = f'{domain}_cookies.txt'
+    try:
+        cookiejar = MozillaCookieJar()
+        cookiejar.load(os.path.join(cookie_home, filename))
+    except FileNotFoundError as error:
+        raise FileNotFoundError(f'{filename} not exists.') from error
+    else:
+        return {getattr(c, 'name'): getattr(c, 'value') for c in cookiejar}

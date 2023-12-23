a17b07202c4b72467df3c4b55e5f36b426669941
wen <wenslife@outlook.com>
2023-12-23 09:55:12 +0800

    fix(spiders/lemon): fix domain

--- src/favorites_crawler/constants/endpoints.py
+++ src/favorites_crawler/constants/endpoints.py
@@ -7,8 +7,8 @@ YANDERE_LIST_POST_URL = 'https://yande.re/post.json'
 YANDERE_SHOW_POST_URL = 'https://yande.re/post/show/{id}'
 YANDERE_VOTE_POST_URL = 'https://yande.re/post/vote.json'
 
-LEMON_PIC_USER_CENTER_URL = 'https://www.cybesx.com/user-center'
-LEMON_PIC_POST_URL_PATTERN = 'https://www.lmmbtc.com/{id}.html'
+LEMON_PIC_USER_CENTER_URL = 'https://www.emonl.com/user-center'
+LEMON_PIC_POST_URL_PATTERN = 'https://www.emonl.com/{id}.html'
 
 NHENTAI_LOGIN_URL = 'https://nhentai.net/login/?next=/favorites/'
 NHENTAI_USER_FAVORITES_URL = 'https://nhentai.net/favorites/'
